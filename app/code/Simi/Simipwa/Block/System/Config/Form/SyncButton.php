c1b7b2b0e17daa6dd12ec5c9d354f47802f9bdaf
Cody Nguyen <cody@simicart.com>
2018-07-02 13:57:42 +0700

    Update dependences

--- app/code/Simi/Simipwa/Block/System/Config/Form/SyncButton.php
+++ app/code/Simi/Simipwa/Block/System/Config/Form/SyncButton.php
@@ -51,31 +51,60 @@ class SyncButton extends Field
             <script type="text/javascript">
                 function addHomeScreenWarning() {
                     simipwa_notification_enable = document.getElementById("simipwa_notification_enable");
-                    if(simipwa_notification_enable.value == 0) {
+                    simipwa_general_pwa_enable = document.getElementById("simipwa_general_pwa_enable");
+                    
+                    if(simipwa_notification_enable.value == 0 &&
+                     simipwa_general_pwa_enable && 
+                     typeof simipwa_general_pwa_enable != "undefined" &&
+                     simipwa_general_pwa_enable.value == 0) {
+                        updateDisplayHomeScreenFields(false);
                         addToHomeWarning = document.getElementById("add_to_home_warning");
                         if (!addToHomeWarning || typeof addToHomeWarning == "undefined") {
                             homescreen_enable = document.getElementById("row_simipwa_homescreen_homescreen_enable");
                             var addToHomeWarning = document.createElement("div");
-                                addToHomeWarning.innerHTML = "Please enable Offline Mode to open Add to Home Screen feature";
-                                addToHomeWarning.className = "add_to_home_warning";
-                                addToHomeWarning.id = "add_to_home_warning";
+                            addToHomeWarning.innerHTML = "Please enable Offline Mode to open Add to Home Screen feature";
+                            addToHomeWarning.className = "add_to_home_warning";
+                            addToHomeWarning.id = "add_to_home_warning";
                             homescreen_enable.parentNode.insertBefore(addToHomeWarning, homescreen_enable);   
                         } else {
                             addToHomeWarning.style.display = "block";
                         }
                     } else {
                         addToHomeWarning = document.getElementById("add_to_home_warning");
+                        updateDisplayHomeScreenFields(true);
                         if (addToHomeWarning && typeof addToHomeWarning != "undefined")
                             addToHomeWarning.style.display = "none";
                     }
                 }
                 
+                function updateDisplayHomeScreenFields(display) {
+                    if (!display) {
+                        document.getElementById("row_simipwa_homescreen_homescreen_enable").style.display = "none";
+                        document.getElementById("row_simipwa_homescreen_app_name").style.display = "none";
+                        document.getElementById("row_simipwa_homescreen_app_short_name").style.display = "none";
+                        document.getElementById("row_simipwa_homescreen_home_screen_icon").style.display = "none";
+                        document.getElementById("row_simipwa_homescreen_theme_color").style.display = "none";
+                        document.getElementById("row_simipwa_homescreen_background_color").style.display = "none";
+                    } else {
+                        document.getElementById("row_simipwa_homescreen_homescreen_enable").style.removeProperty("display");
+                        document.getElementById("row_simipwa_homescreen_app_name").style.removeProperty("display");
+                        document.getElementById("row_simipwa_homescreen_app_short_name").style.removeProperty("display");
+                        document.getElementById("row_simipwa_homescreen_home_screen_icon").style.removeProperty("display");
+                        document.getElementById("row_simipwa_homescreen_theme_color").style.removeProperty("display");
+                        document.getElementById("row_simipwa_homescreen_background_color").style.removeProperty("display");
+                    }
+                }
+                
                 document.addEventListener("DOMContentLoaded", function(event) {
-                    addHomeScreenWarning();
                     var simipwa_notification_enable = document.getElementById("simipwa_notification_enable");
                     simipwa_notification_enable.addEventListener("change", function() {
                         addHomeScreenWarning();
                     });
+                    simipwa_general_pwa_enable = document.getElementById("simipwa_general_pwa_enable");
+                    simipwa_general_pwa_enable.addEventListener("change", function() {
+                        addHomeScreenWarning();
+                    });
+                    addHomeScreenWarning();
                 });
             </script>
         ';
