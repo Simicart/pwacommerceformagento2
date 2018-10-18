240b5a5a4cb154ceb9e6f2665a6c23d9dbf8411c
Cody Nguyen <cody@simicart.com>
2018-10-18 15:17:33 +0700

    Fix upgrade issue

--- app/code/Simi/Simipwa/Setup/UpgradeSchema.php
+++ app/code/Simi/Simipwa/Setup/UpgradeSchema.php
@@ -1,43 +1,41 @@
 <?php
 
-namespace Simi\Simistorelocator\Setup;
+/**
+ * Copyright © 2018 Simi. All rights reserved.
+ */
+
+namespace Simi\Simipwa\Setup;
 
 use Magento\Framework\Setup\UpgradeSchemaInterface;
-use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
-use Magento\Framework\DB\Adapter\AdapterInterface;
-use Simi\Simistorelocator\Setup\InstallSchema as StorelocatorShema;
+use Magento\Framework\Setup\ModuleContextInterface;
 
 class UpgradeSchema implements UpgradeSchemaInterface
 {
-
-    /**
-     * {@inheritdoc}
-     */
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        $installer = $setup;
-        $installer->startSetup();
+        $setup->startSetup();
 
-        if (version_compare($context->getVersion(), '1.0.1', '<')) {
-            $this->addNewTableCustomerMap($installer);
+        //handle all possible upgrade versions
+
+        if(!$context->getVersion()) {
+            //no previous version found, installation, InstallSchema was just executed
+            //be careful, since everything below is true for installation !
         }
 
-        $installer->endSetup();
-    }
+        if (version_compare($context->getVersion(), '1.0.1') < 0) {
+            $setup->run("DROP TABLE IF EXISTS {$setup->getTable('simipwa_social_customer_mapping')};");
+            $setup->run("
+                CREATE TABLE {$setup->getTable('simipwa_social_customer_mapping')} (
+                    `id` int(11) unsigned NOT NULL auto_increment,
+                    `customer_id` int(11) NULL default 0,
+                    `social_user_id` VARCHAR(255) NULL DEFAULT  '',
+                    `provider_id` VARCHAR(255) NULL DEFAULT  '',
+                    PRIMARY KEY (`id`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+            ");
+        }
 
-    public function addNewTableCustomerMap(SchemaSetupInterface $installer)
-    {
-        $installer->run("DROP TABLE IF EXISTS {$installer->getTable('simipwa_social_customer_mapping')};");
-        $installer->run("
-            CREATE TABLE {$installer->getTable('simipwa_social_customer_mapping')} (
-                `id` int(11) unsigned NOT NULL auto_increment,
-                `customer_id` int(11) NULL default 0,
-                `social_user_id` VARCHAR(255) NULL DEFAULT  '',
-                `provider_id` VARCHAR(255) NULL DEFAULT  '',
-                PRIMARY KEY (`id`)
-                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-        ");
+        $setup->endSetup();
     }
-
 }
