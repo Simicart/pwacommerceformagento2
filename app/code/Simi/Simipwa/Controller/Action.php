d6d1115da8cb1dd989914ed3560974b2c198a93c
Cody Nguyen <cody@simicart.com>
2018-12-13 09:29:22 +0700

    Avoid form key issue

--- app/code/Simi/Simipwa/Controller/Action.php
+++ app/code/Simi/Simipwa/Controller/Action.php
@@ -34,8 +34,12 @@ class Action extends \Magento\Framework\App\Action\Action
 
         // Read Magento\Framework\App\Request\CsrfValidator for reason
         if ($this->getRequest() && $this->getRequest()->isPost()) {
-            $formKey = $this->simiObjectManager->get('\Magento\Framework\Data\Form\formKey')->getFormKey();
-            $this->getRequest()->setParam('form_key', $formKey);
+            try {
+                $formKey = $this->simiObjectManager->get('\Magento\Framework\Data\Form\formKey')->getFormKey();
+                $this->getRequest()->setParam('form_key', $formKey);
+            } catch (\Exception $e) {
+                
+            }
         }
     }
     public function execute()
